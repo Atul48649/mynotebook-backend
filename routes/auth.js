1bd09b484bc5c02dbe78f4ec5d63aa18b2064adc
Atul Kumar <atul48649@gmail.com>
2024-02-29 15:44:44 +0530

    add bcryptjs to encrypt password

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,26 @@ const router = express.Router();
 const User = require('../models/User');
 const userSchema = require('../validation/userSchema');
 const validateSchema = require('../middlewares/validateSchema')
+const bcryptjs = require('bcryptjs')
 
 router.post('/createUser', validateSchema(userSchema), async (req, res) => {
     try {
-        console.log(req.body);
         const data = await User.find({ email: req.body.email })
         if (data.length !== 0) {
             res.status(400).json({
                 message: "This email has already been registered please use new email."
             })
         }
-        const user = await User.create(req.body);
+
+        const { name, email, password } = req.body;
+        const salt = await bcryptjs.genSalt(10);
+        const secretPassword = await bcryptjs.hash(password, salt);
+
+        const user = await User.create({
+            name,
+            email,
+            password: secretPassword
+        });
         res.status(200).json({
             data: user
         })
